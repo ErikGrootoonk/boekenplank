0611db5f9214fd56f8c3e6689e7873ae3102b457
ErikG <erik.grootoonk@gmail.com>
2025-04-13 22:23:27 +0200

    models and views created

--- boekenplank/boekenlijst/views.py
+++ boekenplank/boekenlijst/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Book
+
 
 # Create your views here.
 
 def boekenlijst(request):
     
     template = loader.get_template('boekenlijst/boekenlijst.html')
-    context = {
-        'boeken': [
-            {'titel': 'Walkaway', 'auteur': 'Cory Doctorow'},
-            {'titel': 'Boek 2', 'auteur': 'Auteur 2'},
-            {'titel': 'Boek 3', 'auteur': 'Auteur 3'},
-        ],
+    context = { 
+        'boeken': Book.objects.all().values('title', 'author', 'description', 'language', 'cover_image')
     }
+
     return HttpResponse(template.render(context, request))
